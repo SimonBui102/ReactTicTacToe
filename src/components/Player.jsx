b0c706fa50fafd69cab84fc1ea2ea4616f956047
SimonBui <simonbui1999@gmail.com>
2024-11-19 13:32:18 +1100

    Add GameBoard

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,40 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
 
+    const [playerName, setPlayerName]= useState(initialName)
     const [isEditing, setEditing]= useState(false);
 
     function handleEditClick(){
 
         setEditing((editing) => !editing);
     }
+
+    //Event object from onChange
+    //React will give us event object
+    function handleChange(event) {
+
+        setPlayerName(event.target.value);
+
+    }
+
+
     
-    let playerName =<span className="player-name"> {name} </span>
+    let editablePlayerName =<span className="player-name"> {playerName} </span>
 
     let btnCaption = "Edit";
 
     if(isEditing){
-        playerName= <input type = "text" required value = {name}></input>
+        // Onchange will be triggered for every Keystroke
+        // Will provide us with an event object that was entered by the user.
+        editablePlayerName= <input type = "text" required value = {playerName} onChange={handleChange} ></input>
         btnCaption = "Save";
     }
 
     return (
       <li>
         <span className="player">
-            {playerName}
+            {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
         </span>
         <button onClick={() => handleEditClick()}> {btnCaption}</button>
